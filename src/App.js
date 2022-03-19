3493ebc76c7b5c56c34eacc6848a046ea760d6ff
Riku Rainio <riku.rainio@tuni.fi>
2022-03-19 20:30:15 +0200

    add chakra-ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { Badge } from '@chakra-ui/react'
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>this is a test paragraph.</p>
+        <Badge fontSize={"20px"}>test Badge</Badge>
         <a
           className="App-link"
           href="https://reactjs.org"
